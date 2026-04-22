a00a86c15ca6a527ed5d2d82e2d4d6f11b1ac54f
Javier <javieromar003@gmail.com>
2026-04-22 16:48:50 -0500

    tareas

---
+++ semana04/ejercicios C#/ejercicio6/ejercicio6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa el promedio del estudiante (0-20): ");
+            if (int.TryParse(Console.ReadLine(), out int promedio))
+            {
+                if (promedio < 0 || promedio > 20)
+                {
+                    Console.WriteLine("Valor inválido");
+                }
+                else if (promedio >= 0 && promedio <= 5)
+                {
+                    Console.WriteLine("Deficiente");
+                }
+                else if (promedio >= 6 && promedio <= 10)
+                {
+                    Console.WriteLine("Regular");
+                }
+                else if (promedio >= 11 && promedio <= 14)
+                {
+                    Console.WriteLine("Bueno");
+                }
+                else if (promedio >= 15 && promedio <= 20)
+                {
+                    Console.WriteLine("Excelente");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingresa un número válido.");
+            }
+        }
+    }
+}
